d19da604119d9fad353d7709486faf21c2bc34af
Mark Lacomber <marklacomber@gmail.com>
2011-08-31 14:11:06 +0100

    simplified push-queue

--- Push-Queue/logentries.py
+++ Push-Queue/logentries.py
@@ -9,12 +9,46 @@
 
 from google.appengine.ext import webapp
 from google.appengine.api import urlfetch
+from google.appengine.api import taskqueue
 
-import logging, le
+import logging
 
 def init(key, location):
 	if len(logging.getLogger('').handlers) <= 1:
-		logging.getLogger('').addHandler(le.PushQueue(key, location))
+		logging.getLogger('').addHandler(PushQueue(key, location))
+
+
+class PushQueue(logging.Handler):
+
+    def __init__(self, key, location):
+
+        logging.Handler.__init__(self)
+        self.addr = 'https://api.logentries.com/%s/hosts/%s/?realtime=1' %(key, location)
+	format = logging.Formatter('%(asctime)s : %(levelname)s, %(message)s', '%a %b %d %H:%M:%S %Z %Y')
+	self.setFormatter(format)
+  
+
+    def send(self, msg):
+
+        taskqueue.add(url='/logentriesworker', params={'msg':msg, 'addr':self.addr})
+
+
+    def handleError(self, record):
+        pass
+
+
+    def emit(self, record):
+
+	msg = self.format(record)
+        self.send(msg+'\n')
+
+
+    def flush(self):
+	pass
+
+    def close(self):
+
+        logging.Handler.close(self)
 
 
 class LogentriesWorker(webapp.RequestHandler):

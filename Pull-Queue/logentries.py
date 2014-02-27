003fcbcaea9419ab4a4c225d771abd123bf73d25
stumitchell <github@stuartmitchell.com>
2014-02-27 15:07:56 +1300

    Removed tabs from file

--- Pull-Queue/logentries.py
+++ Pull-Queue/logentries.py
@@ -13,8 +13,8 @@ import logging, random
 from google.appengine.api import taskqueue
 
 def init(key, location):
-	if len(logging.getLogger('').handlers) <= 1:
-		logging.getLogger('').addHandler(PullQueue(key, location))
+    if len(logging.getLogger('').handlers) <= 1:
+        logging.getLogger('').addHandler(PullQueue(key, location))
 
 class PullQueue(logging.Handler):
 
@@ -22,18 +22,18 @@ class PullQueue(logging.Handler):
 
         logging.Handler.__init__(self)
         self.addr = '%s/hosts/%s' %(key, location)
-	self.queue = taskqueue.Queue('logentries-pull-queue')
-	format = logging.Formatter('%(asctime)s : %(levelname)s, %(message)s', '%a %b %d %H:%M:%S %Z %Y')
-	self.setFormatter(format)
+        self.queue = taskqueue.Queue('logentries-pull-queue')
+        format = logging.Formatter('%(asctime)s : %(levelname)s, %(message)s', '%a %b %d %H:%M:%S %Z %Y')
+        self.setFormatter(format)
 
 
     def send(self, msg):
-	try:
-		task = taskqueue.Task(method='PULL', name = str(random.randrange(1,10000000000)), params={'msg':msg, 'addr':self.addr})
-		self.queue.add(task)
-	except apiproxy_errors.OverQuotaError, message:
-		logging.error(message)
-		logging.error("URLFetch API Quota reached, unable to transmit logs to Logentries") 
+        try:
+            task = taskqueue.Task(method='PULL', name = str(random.randrange(1,10000000000)), params={'msg':msg, 'addr':self.addr})
+            self.queue.add(task)
+        except apiproxy_errors.OverQuotaError, message:
+            logging.error(message)
+            logging.error("URLFetch API Quota reached, unable to transmit logs to Logentries") 
 
 
     def handleError(self, record):
@@ -41,16 +41,14 @@ class PullQueue(logging.Handler):
 
 
     def emit(self, record):
-
-	msg = self.format(record)
+        msg = self.format(record)
         self.send(msg+'\n')
 
 
     def flush(self):
-	pass
+        pass
 
     def close(self):
-
         logging.Handler.close(self)
 
 

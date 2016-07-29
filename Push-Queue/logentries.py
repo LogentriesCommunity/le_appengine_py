37d0bfb62c75a008c0c3082061b70d416f560ccd
Maximillian Dornseif <m.dornseif@hudora.de>
2016-07-29 11:54:15 +0200

    Avoid loops if transmission to api.logentries.com fails (#3)
    * Removed tabs from file / fixed import
    
    * avoid loops
    
    if the logging function is logging in case of
    errors we get nasty loops.
    
    This patch breaks the loop by avoiding the
    Python logging infrastructure and dumping
    errors happened during logging just so
    STDERR which will go into the appengine logs.

--- Push-Queue/logentries.py
+++ Push-Queue/logentries.py
@@ -9,24 +9,30 @@
 from google.appengine.ext import webapp
 from google.appengine.api import urlfetch
 from google.appengine.api import taskqueue
+from google.appengine.runtime import apiproxy_errors
 
 import logging
+import sys
 
 def init(key, location):
-	if len(logging.getLogger('').handlers) <= 1:
-		logging.getLogger('').addHandler(PushQueue(key, location))
+    if len(logging.getLogger('').handlers) <= 1:
+        logging.getLogger('').addHandler(PushQueue(key, location))
 
 class LogentriesWorker(webapp.RequestHandler):
 
-   	def post(self):
-      		rpc = urlfetch.create_rpc()
-      		msg = self.request.get('msg')
-      		addr = self.request.get('addr')
-		try:
-      			urlfetch.make_fetch_call(rpc, addr, payload = msg, method=urlfetch.PUT, headers={'content-length':str(len(msg))})
-		except apiproxy_errors.OverQuotaError, message:
-			logging.error(message)
-			logging.error("URLFetch API Quota reached, unable to transmit logs to Logentries")
+    def post(self):
+        rpc = urlfetch.create_rpc()
+        msg = self.request.get('msg')
+        addr = self.request.get('addr')
+        try:
+            urlfetch.make_fetch_call(rpc, addr, payload = msg, method=urlfetch.PUT, headers={'content-length':str(len(msg))})
+            # for debugging, enable this:
+            # result = urlfetch.fetch(addr, payload = msg, method=urlfetch.PUT, headers={'content-length':str(len(msg))})
+            # if result.status_code != 200:
+            #     logging.error("%r", result.content)
+        except apiproxy_errors.OverQuotaError, message:
+            sys.stderr.write(message + '\n')
+            sys.stderr.write("URLFetch API Quota reached, unable to transmit logs to Logentries\n")
 
 
 class PushQueue(logging.Handler):
@@ -35,36 +41,26 @@ class PushQueue(logging.Handler):
 
         logging.Handler.__init__(self)
         self.addr = 'https://api.logentries.com/%s/hosts/%s/?realtime=1' %(key, location)
-	format = logging.Formatter('%(asctime)s : %(levelname)s, %(message)s', '%a %b %d %H:%M:%S %Z %Y')
-	self.setFormatter(format)
-  
+        format = logging.Formatter('%(asctime)s : %(levelname)s, %(message)s', '%a %b %d %H:%M:%S %Z %Y')
+        self.setFormatter(format)
+
 
     def send(self, msg):
-	try:
-        	taskqueue.add(queue_name='logentries-push-queue', url='/logentriesworker', params={'msg':msg, 'addr':self.addr})
-	except apiproxy_errors.OverQuotaError, message:
-		logging.error(message)
-		logging.error("TaskQueue API Quota reached, unable to transmit logs to Logentries")
+        try:
+            taskqueue.add(queue_name='logentries-push-queue', url='/logentriesworker', params={'msg':msg, 'addr':self.addr})
+        except apiproxy_errors.OverQuotaError, message:
+            sys.stderr.write(message + '\n')
+            sys.stderr.write("TaskQueue API Quota reached, unable to transmit logs to Logentries\n")
 
     def handleError(self, record):
         pass
 
-
     def emit(self, record):
-
-	msg = self.format(record)
+        msg = self.format(record)
         self.send(msg+'\n')
 
-
     def flush(self):
-	pass
+        pass
 
     def close(self):
-
         logging.Handler.close(self)
-
-
-
-
-
-
